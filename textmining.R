0e0bde072dc98a27c2bbfd1a6faafb2f89014fa9
Ewan Buckingham <ewan@ewanbuckingham.com>
2020-04-30 13:48:20 +0100

    Moved to Gutenberg examples in the second half of the chapter.

--- textmining.R
+++ textmining.R
@@ -43,4 +43,49 @@ original_books <- austen_books() %>%
 tidy_books <- original_books %>%
   unnest_tokens(word, text)
 
-tidy_books
+#Remove stop words (typically 'as, the, of, and', etc.)
+
+#This shows the contents of the dataset 'stop words'. 
+data(stop_words)
+
+#The stop_words dataset is used for an anti_join that removes them from the tidy_books tibble.
+tidy_books <- tidy_books %>%
+  anti_join(stop_words)
+
+#Use dplyr's count function to find the most common words across all books.
+tidy_books %>%
+  count(word, sort = TRUE) 
+
+#Plot teh frequency of the most common words (greater than 600 instances) using ggplot2. ggplot treats strings as factors (even though
+#they're characters in the tibble, so mutate is used to reorder the column in descending order.
+library(ggplot2)
+
+tidy_books %>%
+  count(word, sort = TRUE) %>%
+  mutate(word = reorder(word, n)) %>%
+  filter(n > 600) %>%
+  ggplot(aes(word, n)) +
+  geom_col() +
+  xlab(NULL) +
+  coord_flip()
+
+
+#Project Gutenberg texts. 
+
+library(gutenbergr)
+
+#Select and download texts from Project Gutenberg by their ID. This returns the book id and a section of text for each row
+#presumably relating to the page width of the original printed book.
+
+hgwells <- gutenberg_download(c(35, 36, 5230, 159))
+
+#Using 'word' as the column name is convenient because it matches with the column name in the stop_words dataset. This makes the
+#anti_join simpler. 
+
+tidy_hgwells <- hgwells %>%
+  unnest_tokens(word, text) %>%
+  anti_join(stop_words)
+
+tidy_hgwells %>%
+  count(word, sort = TRUE)
+
