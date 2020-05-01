b7fe3a8cc9bc2b32414d7b1b33ea88625c6c4349
Ewan Buckingham <ewan@ewanbuckingham.com>
2020-05-01 12:04:37 +0100

    Fitting geom_ablines (for power law) onto plots.

--- textmining-3.R
+++ textmining-3.R
@@ -26,5 +26,55 @@ ggplot(book_words, aes(n/total, fill = book)) +
   xlim(NA, 0.0009) +
   facet_wrap(~book, ncol = 2, scales = "free_y")
 
+#Try to do the same thing as a column plot
+#Need to create the calculated value (tf). 
+#Will also need to crop the dataset to just 20 values so they can be displayed. 
+#
+#included_words <- c("the", "to", "and", "of", "her", "a", "i", "in", "was", "it")
+#library(forcats)
+#
+#book_words <- book_words %>%
+#  mutate(tf = n/total) %>%
+#  group_by(book) %>%
+#  filter(word %in% included_words) %>%
+#  mutate(word = fct_reorder(word, desc(tf))) %>%
+#  ungroup
+#  
+#
+#  
+#ggplot(book_words, aes(word, tf, fill = book)) +
+#  geom_col(show.legend = TRUE) + 
+#  facet_wrap(~book)
+#
+#####n.b. This code asks ggplot to do something it can't do. Rank the words by term frequency and then plot
+#####smoothly descending curves for each. That's not possible because some words are used more than others (i.e.
+##### 'to' is not the most consistently used word. For some books it's 'the'. So the factor reorder doesn't work.)
+#####I tried using both mutate(word = reorder(word, tf) and forecats::fct_re_order(word, tf) before realising.)
 
+frequency_by_rank <- book_words %>%
+  group_by(book) %>%
+  mutate(rank = row_number(), `term frequency` = n/total)
 
+#Plotting Zipf's Law - on a log scale an inversely proportional relationship should have a constant slope. 
+
+#Be careful with quotes. `term frequency` is correct. 'term frequency' works in mutate but not in ggplot and is incorrect.
+ggplot(frequency_by_rank, aes(rank, `term frequency`, colour = book)) +
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) +
+  scale_x_log10() +
+  scale_y_log10()
+
+#Viewing the curve as a series of power law calculations 
+
+rank_subset <- frequency_by_rank %>% 
+  filter(rank < 500,
+         rank > 10)
+
+lm(log10(`term frequency`) ~ log10(rank), data = rank_subset)
+
+#Overlaying the fitted power law onto the plots from the books.
+frequency_by_rank %>% 
+  ggplot(aes(rank, `term frequency`, color = book)) + 
+  geom_abline(intercept = -0.62, slope = -1.1, color = "gray50", linetype = 2) +
+  geom_line(size = 1.1, alpha = 0.8, show.legend = FALSE) + 
+  scale_x_log10() +
+  scale_y_log10()
